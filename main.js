e085773cbe8b9218dfc6fac870790dc15c95de8f
Fabien Marceau <fabien.marceau@gmail.com>
2023-03-17 11:25:45 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,79 @@
+for (let i = 0; i < 100; i++) {
+  var snowflake = document.createElement("div");
+  var snowflakeA = document.createElement("div");
+  var snowflakeB = document.createElement("div");
+
+  snowflake.classList.add("snowflake");
+  snowflakeA.classList.add("snowflakeA");
+  snowflakeB.classList.add("snowflakeB");
+
+  snowflake.style.left = Math.random() * window.innerWidth + "px";
+  snowflakeA.style.left = Math.random() * window.innerWidth + "px";
+  snowflakeB.style.left = Math.random() * window.innerWidth + "px";
+
+  snowflake.style.top = Math.random() * window.innerHeight + "px";
+  snowflakeA.style.top = Math.random() * window.innerHeight + "px";
+  snowflakeB.style.top = Math.random() * window.innerHeight + "px";
+
+  document.querySelector(".snowflakes").appendChild(snowflake);
+  document.querySelector(".snowflakes").appendChild(snowflakeA);
+  document.querySelector(".snowflakes").appendChild(snowflakeB);
+}
+
+for (let i = 0; i < 30; i++) {
+  const barricades = document.createElement("div");
+  barricades.classList.add("barricades");
+  barricades.style.left = i * 40 + "px";
+  barricades.style.top = "-7px";
+  document.querySelector(".road").appendChild(barricades);
+}
+
+const tm = gsap.timeline();
+
+gsap.to(".snowflake", {
+  duration: 10,
+  ease: "sine",
+  y: "+=500px",
+  repeat: -1,
+});
+
+gsap.to(".snowflakeA", {
+  duration: 10,
+  ease: "sine",
+  y: "+=500px",
+  repeat: -1,
+});
+
+gsap.to(".snowflakeB", {
+  duration: 10,
+  ease: "sine",
+  delay: 1,
+  y: "+=500px",
+  repeat: -1,
+});
+
+gsap.to(".mountain", {
+  scale: 2,
+  duration: 30,
+  yoyo: true,
+  repeat: -1,
+  borderBottomColor: "orange",
+  delay: 3,
+  ease: "sine",
+});
+
+gsap.to(".panorama_A", {
+  backgroundImage: "linear-gradient(to right, #ff7f50, #ffd700)",
+  duration: 30,
+  ease: "sine",
+  repeat: -1,
+  yoyo: true,
+  delay: 3,
+});
+
+tm.to(".car", {
+  x: 800,
+  duration: 40,
+  repeat: -1,
+  ease: "power",
+});
